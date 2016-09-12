9e4027a7638400bc94126b39ef09ea4ae642be7a
JustAnAlais <lbsmusicprod@gmail.com>
2016-09-12 08:46:15 +0200

    Server.js

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
 /**
  * Created by Rasmus on 12-09-2016.
  */
-
+// Import required libraries
 require(__dirname + '/Resources/config.js');
+var fs = require('fs');
+var net = require('net');
 
-console.log(config.database);
+// Load Initializer
+var init_files = fs.readdirSync(__dirname + "/Initializer");
+init_files.forEach(function (initFile) {
+    console.log('Loading initializer:' + initFile);
+    require(__dirname + "/Initializer" + initFile);
+});
+
+var model_files = fs.readdirSync(__dirname + "/Models");
+model_files.forEach(function (modelFile) {
+    console.log('Loading Model:' + modelFile);
+    require(__dirname + "/Models" + modelFile);
+});
+
+maps = {};
+var map_files = fs.readdirSync(config.data_path.maps);
+map_files.forEach(function (mapFile) {
+    console.log('Loading Map:' + mapFile);
+    var map = require(config.data_path + mapFile);
+    maps[map.room] = map
+});
+
+
+
+// Load Data Models
+// Load Game Maps data
+// Initiate server
+
