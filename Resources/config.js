ed96fc832ed4f93f9fd32be917c0d9a2bcb1924e
JustAnAlais <lbsmusicprod@gmail.com>
2016-09-12 08:33:14 +0200

    Initial setup

---
+++ Resources/config.js
@@ -0,0 +1,42 @@
+
+// Import required libraries
+var args = require('minimist')(process.argv.slice(2));
+var extend = require('extend');
+
+// Store environment variable
+var environment = args.env || "test";
+
+// Common Config
+var common_conf = {
+    name: "SemPro game Server",
+    version: "",
+    environment: environment,
+    max_player: 100,
+    data_path: {
+        items: __dirname + "\\Game Data\\" + "Items\\",
+        maps: __dirname + "\\Game Data\\" + "Maps\\",
+
+    },
+    starting_zone: "rm_map_home"
+
+};
+
+// Environment specific config
+var conf = {
+    production: {
+        ip: args.ip || "0.0.0.0",
+        port: args.port || 8081,
+        database: "mongodb://127.0.0.1/mmosrv_prod"
+    },
+    test: {
+        ip: args.ip || "0.0.0.0",
+        port: args.port || 8082,
+        database: "mongodb://127.0.0.1/mmosrv_test"
+    }
+
+};
+
+extend(false, conf.production, common_conf);
+extend(false, conf.test, common_conf);
+
+module.exports = config = conf[environment];
---
+++ server.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Rasmus on 12-09-2016.
+ */
+
+require(__dirname + '/Resources/config.js');
+
+console.log(config.database);
